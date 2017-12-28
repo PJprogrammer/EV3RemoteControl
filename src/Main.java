513659d7768a3e9238c3b04968d0d576bb1369fa
PJprogrammer <pauljprogrammer@gmail.com>
2017-12-28 02:40:14 -0500

    New Method of Finding EV3

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ import lejos.remote.ev3.RemoteEV3;
 import net.java.games.input.*;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 public class Main {
@@ -37,7 +39,15 @@ public class Main {
 
     public void connectToEV3(){
         // Detecting EV3 Brick
-        ev3 = (RemoteEV3) BrickFinder.getDefault();
+        try {
+            ev3 = new RemoteEV3("10.0.0.24");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
 
         // Creating objects for motor
         leftMotor = ev3.createRegulatedMotor("B", 'L');
